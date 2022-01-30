f7e833107ce594a540aa75207842c0a0ed26827f
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-01-30 17:29:14 -0600

    date-day-weather

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,23 +1,30 @@
-const cityName = document.querySelector('#cityName');
-const weatherIcon = document.querySelector('#weather-icon');
-const weatherTemp = document.querySelector('#weather-temp');
+const $cityName = document.querySelector('#cityName');
+const $weatherIcon = document.querySelector('#weather-icon');
+const $weatherTemp = document.querySelector('#weather-temp');
+const $greeting = document.querySelector('#greeting');
+
+const daysOfWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const baseUrl = 'http://api.openweathermap.org/';
 
 const getWeatherHuamantla = async ()=>{
-    const res = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Huamantla&appid=25745ce0da2954b896c845ab208d6851&units=metric');
+    const res = await axios.get(`${baseUrl}data/2.5/weather?q=Huamantla&appid=25745ce0da2954b896c845ab208d6851&units=metric`);
     const data = res.data;
     const weather = {
         desc: data.weather[0].description,
         icon: data.weather[0].icon,
         temp: `${data.main.temp}º`,
-        feels: `feels like ${data.main.feels_like}`
+        feels: `feels like ${data.main.feels_like}`,
+        date: new Date()
     }
     const icon = weather.icon
+    const day = weather.date.getDay();
 
-    cityName.textContent = `${data.name}, ${data.sys.country}`;
-    weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-    weatherTemp.childNodes[1].textContent = weather.temp;
-    weatherTemp.childNodes[3].textContent = weather.feels;
-    console.log(new Date.now);
+    $cityName.textContent = `${data.name}, ${data.sys.country}`;
+    $weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    $weatherTemp.childNodes[1].textContent = weather.temp;
+    $weatherTemp.childNodes[3].textContent = weather.feels;
+    $greeting.childNodes[3].textContent = daysOfWeek[day];
 }
 
 getWeatherHuamantla();
